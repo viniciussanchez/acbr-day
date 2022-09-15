2ad027ffaea27e64eac9de13ec7b77b63e344b92
Vinicius Sanchez <viniciuss.sanchez@gmail.com>
2022-09-14 23:38:04 -0300

    api nfse

--- 2022/api-nfse-horse-acbr/src/controllers/Controllers.NFSe.pas
+++ 2022/api-nfse-horse-acbr/src/controllers/Controllers.NFSe.pas
@@ -4,19 +4,7 @@ interface
 
 implementation
 
-uses Horse, Services.NFSe, System.JSON, REST.Json;
-
-procedure DoCancelarNFSe(Req: THorseRequest; Res: THorseResponse);
-var
-  LService: TServiceNFSe;
-begin
-  LService := TServiceNFSe.Create;
-  try
-
-  finally
-    LService.Free;
-  end;
-end;
+uses Horse, Services.NFSe, System.JSON, REST.Json, ACBrNFSeXWebserviceBase;
 
 procedure DoGetNFSeTomador(Req: THorseRequest; Res: THorseResponse);
 var
@@ -54,13 +42,30 @@ begin
   end;
 end;
 
-procedure DoGetLinkNFSe(Req: THorseRequest; Res: THorseResponse);
+procedure DoCancelarNFSe(Req: THorseRequest; Res: THorseResponse);
 var
   LService: TServiceNFSe;
+  LInformacoesCancelamento: TInfCancelamento;
 begin
   LService := TServiceNFSe.Create;
   try
+    LInformacoesCancelamento := TJson.JsonToObject<TInfCancelamento>(Req.Body);
+    Res.Send<TJSONObject>(TJson.ObjectToJsonObject(LService.CancelarNFSe(LInformacoesCancelamento)));
+  finally
+    LService.Free;
+  end;
+end;
 
+procedure DoGetLinkNFSe(Req: THorseRequest; Res: THorseResponse);
+var
+  LService: TServiceNFSe;
+  LNumeroNFSe, LCodigoVerificacao: string;
+begin
+  LService := TServiceNFSe.Create;
+  try
+    LNumeroNFSe := Req.Query.Field('numero').AsString;
+    LCodigoVerificacao := Req.Query.Field('codigoVerificacao').AsString;
+    Res.Send<TJSONObject>(TJSONObject.Create(TJSONPair.Create('link', LService.GerarLinkNFSe(LNumeroNFSe, LCodigoVerificacao))));
   finally
     LService.Free;
   end;
@@ -69,10 +74,18 @@ end;
 procedure DoSubstituirNFSe(Req: THorseRequest; Res: THorseResponse);
 var
   LService: TServiceNFSe;
+  LNumeroNovoRPS, LNumeroNFSe, LNumeroLote, LNumeroSerie, LCodigoCancelamento, LMotivoCancelamento, LCodigoVerificacao: string;
 begin
   LService := TServiceNFSe.Create;
   try
-
+    LNumeroNovoRPS := Req.Query.Field('rps').AsString;
+    LNumeroNFSe := Req.Query.Field('nfse').AsString;
+    LNumeroLote := Req.Query.Field('lote').AsString;
+    LNumeroSerie := Req.Query.Field('serie').AsString;
+    LCodigoCancelamento := Req.Query.Field('codigoCancelamento').AsString;
+    LMotivoCancelamento := Req.Query.Field('motivo').AsString;
+    LCodigoVerificacao := Req.Query.Field('codigoVerificacao').AsString;
+    Res.Send<TJSONObject>(TJson.ObjectToJsonObject(LService.SubstituirNFSe(LNumeroNovoRPS, LNumeroNFSe, LNumeroLote, LNumeroSerie, LCodigoCancelamento, LMotivoCancelamento, LCodigoVerificacao)));
   finally
     LService.Free;
   end;
@@ -81,10 +94,14 @@ end;
 procedure DoEmitirNFSe(Req: THorseRequest; Res: THorseResponse);
 var
   LService: TServiceNFSe;
+  LQuantidade, LNumeroRPS, LNumeroLote: string;
 begin
   LService := TServiceNFSe.Create;
   try
-
+    LQuantidade := Req.Query.Field('quantidade').AsString;
+    LNumeroRPS := Req.Query.Field('rps').AsString;
+    LNumeroLote := Req.Query.Field('lote').AsString;
+    Res.Send<TJSONObject>(TJson.ObjectToJsonObject(LService.EmitirNFSe(LQuantidade, LNumeroRPS, LNumeroLote)));
   finally
     LService.Free;
   end;
--- 2022/api-nfse-horse-acbr/src/services/Services.NFSe.pas
+++ 2022/api-nfse-horse-acbr/src/services/Services.NFSe.pas
@@ -5,17 +5,72 @@ interface
 uses System.SysUtils, System.Classes, Providers.NFSe, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
   FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.ConsoleUI.Wait,
   FireDAC.Phys.PGDef, ACBrDFeReport, ACBrNFSeXDANFSeClass, ACBrNFSeXDANFSeRLClass, ACBrMail, ACBrBase, ACBrDFe, ACBrNFSeX,
-  FireDAC.Phys.PG, Data.DB, FireDAC.Comp.Client, FireDAC.VCLUI.Wait;
+  FireDAC.Phys.PG, Data.DB, FireDAC.Comp.Client, FireDAC.VCLUI.Wait, ACBrNFSeXConversao, ACBrNFSeXWebservicesResponse,
+  ACBrNFSeXWebserviceBase;
 
 type
   TServiceNFSe = class(TProviderNFSe)
   public
     function GerarLinkNFSe(const ANumeroNFSe, ACodigoVerificacao: string): string;
+    function CancelarNFSe(const AInformacoesCancelamento: TInfCancelamento): TNFSeCancelaNFSeResponse;
+    function EmitirNFSe(const AQuantidade, ANumeroRPS, ANumeroLote: string): TNFSeEmiteResponse;
+    procedure ConsultarNFSeTomador;
+    procedure ConsultarNFSePrestador;
+    procedure ConsultarNFSe;
+    function SubstituirNFSe(const ANumeroNovoRPS, ANumeroNFSe, ANumeroLote, ASerieNFSe, ACodigoCancelamento, ACodigoVerificacao, AMotivoCancelamento: string): TNFSeSubstituiNFSeResponse;
   end;
 
 implementation
 
 {$R *.dfm}
+
+function TServiceNFSe.CancelarNFSe(const AInformacoesCancelamento: TInfCancelamento): TNFSeCancelaNFSeResponse;
+begin
+  try
+    { TODO : Validar informações }
+    ACBrNFSeX.CancelarNFSe(AInformacoesCancelamento);
+    Result := ACBrNFSeX.WebService.CancelaNFSe;
+  finally
+    AInformacoesCancelamento.Free;
+  end;
+end;
+
+procedure TServiceNFSe.ConsultarNFSe;
+begin
+
+end;
+
+procedure TServiceNFSe.ConsultarNFSePrestador;
+begin
+
+end;
+
+procedure TServiceNFSe.ConsultarNFSeTomador;
+begin
+
+end;
+
+function TServiceNFSe.EmitirNFSe(const AQuantidade, ANumeroRPS, ANumeroLote: string): TNFSeEmiteResponse;
+var
+  I, LQuantidade: Integer;
+begin
+  if AQuantidade.Trim.IsEmpty then
+    raise Exception.Create('Informe a quantidade de RPS');
+  if ANumeroRPS.Trim.IsEmpty then
+    raise Exception.Create('Informe o númeero do RPS');
+  if ANumeroLote.Trim.IsEmpty then
+    raise Exception.Create('Informe o número do lote');
+
+  LQuantidade := StrToIntDef(AQuantidade, 1);
+
+  for I := 1 to LQuantidade do
+  begin
+    { TODO : alimentar nfse }
+  end;
+
+  ACBrNFSeX.Emitir(ANumeroLote);
+  Result := ACBrNFSeX.WebService.Emite;
+end;
 
 function TServiceNFSe.GerarLinkNFSe(const ANumeroNFSe, ACodigoVerificacao: string): string;
 begin
@@ -24,6 +79,63 @@ begin
   if ACodigoVerificacao.Trim.IsEmpty then
     raise Exception.Create('Informe o código de verificação');
   Result := ACBrNFSeX.LinkNFSe(ANumeroNFSe, ACodigoVerificacao);
+end;
+
+function TServiceNFSe.SubstituirNFSe(const ANumeroNovoRPS, ANumeroNFSe, ANumeroLote, ASerieNFSe, ACodigoCancelamento, ACodigoVerificacao, AMotivoCancelamento: string): TNFSeSubstituiNFSeResponse;
+var
+  LCodigoCancelamento: string;
+begin
+  if ANumeroNovoRPS.Trim.IsEmpty then
+    raise Exception.Create('Informe o número do novo RPS');
+  if ACodigoCancelamento.Trim.IsEmpty then
+    raise Exception.Create('Informe o código de cancelamento');
+  if ANumeroNFSe.Trim.IsEmpty then
+    raise Exception.Create('Informe o número da NFSe');
+
+  { TODO : alimentar nfse }
+
+  LCodigoCancelamento := ACodigoCancelamento;
+  if ACBrNFSeX.Configuracoes.Geral.Provedor = proSigep then
+  begin
+    case StrToIntDef(ACodigoCancelamento, 1) of
+      1:
+        LCodigoCancelamento := 'EE';
+      2:
+        LCodigoCancelamento := 'ED';
+      3:
+        LCodigoCancelamento := 'OU';
+      4:
+        LCodigoCancelamento := 'SB';
+    end;
+  end;
+
+  if ACBrNFSeX.Configuracoes.Geral.Provedor in [proAgili, proConam, proEquiplano, proGoverna, proIPM, proISSDSF, proISSLencois,
+    proModernizacaoPublica, proPublica, proSiat, proSigISS, proSmarAPD, proWebFisco, proSudoeste] then
+  begin
+    if AMotivoCancelamento.Trim.IsEmpty then
+      raise Exception.Create('Informe o motivo do cancelamento');
+  end;
+
+  if ACBrNFSeX.Configuracoes.Geral.Provedor = proiiBrasil then
+  begin
+    if ASerieNFSe.Trim.IsEmpty then
+      raise Exception.Create('Informe a série da NFSe');
+  end;
+
+  if ACBrNFSeX.Configuracoes.Geral.Provedor = proAssessorPublico then
+  begin
+    if ANumeroLote.Trim.IsEmpty then
+      raise Exception.Create('Informe o número do lote');
+  end;
+
+  if ACBrNFSeX.Configuracoes.Geral.Provedor in [proISSLencois, proGoverna, proSiat, proSigep, proElotech] then
+  begin
+    if ACodigoVerificacao.Trim.IsEmpty then
+      raise Exception.Create('Informe o código de verificação');
+  end;
+
+  ACBrNFSeX.SubstituirNFSe(ANumeroNFSe, ASerieNFSe, LCodigoCancelamento, AMotivoCancelamento, ANumeroLote, ACodigoVerificacao);
+  Result := ACBrNFSeX.WebService.SubstituiNFSe;
 end;
 
 end.
